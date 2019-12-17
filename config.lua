d517cd12c70518a75163f088ae33ec90b48fc79f
VileGoo <43779616+VileGoo@users.noreply.github.com>
2019-12-17 13:51:13 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,3 @@
 MaxHealthIncrease = 0 -- How much the player's max health will be increased by.
-HealAmount = 15 -- How much the player is healed.
-DropChance = 10 -- Higher values means lower chance. Set to 0 for 100% drop chance. Set to 1 for 50% drop chance. etc
+HealAmount = 10 -- How much the player is healed.
+DropChance = 15 -- Higher values means lower chance. Set to 0 for 100% drop chance. Set to 1 for 50% drop chance. etc
